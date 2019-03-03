c7646548f2379a8f986eb21e5c16c24b1bb819c1
Qinanda <34642888+Qinanda@users.noreply.github.com>
2019-03-03 12:20:38 +0800

    Add files via upload

---
+++ whoip.php
@@ -0,0 +1,48 @@
+<?php
+function IPnya() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'IP Tidak Dikenali';
+ 
+    return $ipaddress;
+}
+$ipaddress = $_SERVER['REMOTE_ADDR'];
+//echo "IP address : ";
+//echo IPnya();
+
+    $apiKey = "c3e816b34289468ea74ba2ac6759d313";
+    $ip = IPnya();
+    $location = get_geolocation($apiKey, $ip);
+    $decodedLocation = json_decode($location, true);
+    
+    echo "<pre>";
+    print_r($decodedLocation);
+    echo "</pre>";
+
+    function get_geolocation($apiKey, $ip, $lang = "en", $fields = "*", $excludes = "") {
+        $url = "https://api.ipgeolocation.io/ipgeo?apiKey=".$apiKey."&ip=".$ip."&lang=".$lang."&fields=".$fields."&excludes=currency,time_zone,country_flag,connection_type,organization,is_eu,calling_code,district".$excludes;
+        $cURL = curl_init();
+
+        curl_setopt($cURL, CURLOPT_URL, $url);
+        curl_setopt($cURL, CURLOPT_HTTPGET, true);
+        curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Accept: application/json'
+        ));
+        return curl_exec($cURL);
+    }
+?>
+
